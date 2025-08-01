bed6e84e1d6873d1640941af4acd8a7688b3750f
alisher-erkin <alisher.e.tagaev@gmail.com>
2025-08-01 14:53:43 +0500

    change

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,16 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("node-sass"));
-var del = require("del");
 
-// Очистка dist
-gulp.task("clean", function () {
-  return del(["dist"]);
-});
-
-// Компиляция SCSS в dist/css
-gulp.task("sass", function () {
+gulp.task("sass", async function () {
   return gulp
     .src("scss/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("dist/css")); // ⬅️ Сохраняем в dist
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest("css"));
 });
 
-// Копирование HTML (если есть)
-gulp.task("html", function () {
-  return gulp.src("*.html").pipe(gulp.dest("dist"));
-});
-
-// Копирование JS (если есть)
-gulp.task("js", function () {
-  return gulp.src("js/*.js").pipe(gulp.dest("dist/js"));
-});
-
-// Наблюдение за SCSS
 gulp.task("watch", function () {
-  gulp.watch("scss/*.scss", gulp.series(["sass"]));
+  return gulp.watch("scss/*.scss", gulp.series(["sass"]));
+  // Other watchers
 });
 
-// Финальная сборка
-gulp.task("build", gulp.series("clean", gulp.parallel("sass", "html", "js")));
+gulp.task('build', gulp.series('sass'));
