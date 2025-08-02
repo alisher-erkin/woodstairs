db682f8677c20b2e9e74c424949024fe72b07685
alisher-erkin <alisher.e.tagaev@gmail.com>
2025-08-02 19:21:27 +0500

    chnage

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
 
-var gulp = require("gulp");
-var sass = require("gulp-sass")(require("sass"));
+// Очистка папки dist
+function clean() {
+  return del(['dist']);
+}
 
+// Компиляция SCSS → CSS
+function styles() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+}
 
+// Копирование HTML
+function html() {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+}
 
+const build = gulp.series(clean, gulp.parallel(styles, html));
 
-gulp.task("sass", async function () {
-  return gulp
-    .src("scss/*.scss")
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest("css"));
-});
-
-gulp.task("watch", function () {
-  return gulp.watch("scss/*.scss", gulp.series(["sass"]));
-  // Other watchers
-});
-
-gulp.task('build', gulp.series('sass'));
+exports.clean = clean;
+exports.styles = styles;
+exports.html = html;
+exports.build = build;
 
 
