0294bfb134725113dc63d5ab73f22738a17cc5c9
alisher-erkin <alisher.e.tagaev@gmail.com>
2025-08-03 07:54:24 +0500

    change

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,37 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const del = require("del");
 
-var gulp = require("gulp");
-var sass = require("gulp-sass")(require("sass"));
+// 1. Удаление старой папки dist
+function clean() {
+  return del(["dist"]);
+}
 
-
-
-
-gulp.task("sass", async function () {
+// 2. Компиляция SCSS → dist/css
+function styles() {
   return gulp
     .src("scss/*.scss")
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest("css"));
-});
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/css"));
+}
 
+// 3. Копирование HTML → dist/
+function html() {
+  return gulp.src("*.html").pipe(gulp.dest("dist"));
+}
+
+// 4. Копирование JS (если есть) → dist/js
+function scripts() {
+  return gulp.src("js/**/*.js").pipe(gulp.dest("dist/js"));
+}
+
+// 5. Вот задача, которую вызывает Netlify: gulp build
+gulp.task("build", gulp.series(clean, html, styles, scripts));
+
+// 6. Задача watch (для локальной разработки)
 gulp.task("watch", function () {
-  return gulp.watch("scss/*.scss", gulp.series(["sass"]));
-  // Other watchers
+  gulp.watch("scss/*.scss", styles);
 });
 
-gulp.task('build', gulp.series('sass'));
 
 
